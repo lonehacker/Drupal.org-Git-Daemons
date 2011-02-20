faf8141428ab32f54a6c293fd81f57ca9379f090
Trevor Hardcastle <chizu@spicious.com>
2011-02-20 21:10:40 +0000

    Fix error message format string indices.

--- drupalGitSSHDaemon.py
+++ drupalGitSSHDaemon.py
@@ -173,7 +173,7 @@ class GitSession(object):
                 'git-upload-pack' not in argv[:-1]):
             # First, error out if the project itself is disabled.
             if not auth_service["status"]:
-                error = "Project {1} has been disabled.".format(projectname)
+                error = "Project {0} has been disabled.".format(projectname)
                 return Failure(ConchError(error))
             # If anonymous access for this type of command is not allowed,
             # check if the user is a maintainer on this project
@@ -183,7 +183,7 @@ class GitSession(object):
                 return execGitCommand
             # Username in maintainers list
             elif self.user.username not in users:
-                error = "User '{1}' does not have write permissions for repository '{2}'".format(projectname, self.user.username)
+                error = "User '{1}' does not have write permissions for repository '{0}'".format(projectname, self.user.username)
                 return Failure(ConchError(error))
             elif not user["global"]:
                 # username:key
@@ -194,7 +194,7 @@ class GitSession(object):
                     return execGitCommand
                 else:
                     # Both kinds of username auth failed
-                    error = "Permission denied when accessing '{1}' as user '{2}'".format(projectname, self.user.username)
+                    error = "Permission denied when accessing '{0}' as user '{1}'".format(projectname, self.user.username)
                     return Failure(ConchError(error))
             else:
                 # Account is globally disabled or disallowed
