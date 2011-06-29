de9267237d50f2fe91826f2dbaee24b570abd223
anshul.singhle <anshul.singhle@gmail.com>
2011-06-29 12:59:45 +0530

    Added Stubs for testing

--- tests/tests.py
+++ tests/tests.py
@@ -13,7 +13,7 @@ class TestPubKeyAuth(unittest.TestCase):
     blob = """ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA1qvRgJ/A9TGZAeYuUSQbn7+zrg2Ogs5+ehJ1s/+YE0s1b2m9PT8XkLUceHlMFGkoizHdNbFSJIanSUD91eQXBzMU333vc1ylBjcs1FBcRLyvrXaXsrZhqvnmlMMKhLjb2dn2d1PfmHYznLZGQaxOgMRUoOIJ5rIh3XCQNNbZE3eD/FS7XLWSgfwAFBnYm+uMY0vnPXJLPiNO9iMhekJWM3vKnKNCqyYgaZEIpqSnXekZjafy3uIGhANWKiwVZkLAN47tPxvgu//6IqR5yF59Zmk9JcuhsvsBG+BbddaNXdlQc7P3pY8QDi/7FHcUN9C2A2UYmFMHvpcuvsQ4HJCvOQ== xashck@gmail.com"""
 
     bad_blob = """ssh-rsa AAAAB3NzaC1kc3MAAACBAOsD7WxLruieczHv46el57qKweUJU3R9ECYEDFs799b4BxKnHeJ+UPzC6+BIWqBdJ3+WGMTCMlXpZy3BlcVNdYLRI+AGBmdPW1rQxo0F9xpakFo9h9LKfpKeZz4mWp7FCOgkBmFlvJS+j9GYB/FEOE9LDqx0O7gBdAepm9WOieavAAAAFQDg+VFpV3y0rJbiPvbxj9PGW918ywAAAIAcOlBMKYPdqumG7pdiZtrTYhtwrfulxDy/MOBCcxqyhwuf1hSZZD5xDGbLyI58PLq2asfme1zLU5BSn8TnJJz8ddf3TFFVSJHdE0txuPu8LIfU3DMH2IDa4P/iU0QXlgGIurYeydKklzQg6FeLI+mjqr6LaLhSyBh3bdqW9GCBDwAAAIAa+/R5Bi7u9u7nk+IrAkrUuZciR+HUERBKWLBlCgldSZouziRLq2SkKPEsED2zni80SQxVeIG84olrQlThh35iYsJFNwS4J9vxQ5AJzARMACFYK/EzuFkk2YPV0NfurKd1KUoU8cb+5ns+gwgqWy06/s8jr63cDKWJmm+Hn2RwkQ== demo@demo"""
-    path = os.path.expanduser('~/.ssh/id_rsa')
+    path = os.path.expanduser('../keys/id_rsa')
     privkey = Key.fromFile(path,passphrase='pikachu')
     sigData ="testdata"
     def setUp(self):
